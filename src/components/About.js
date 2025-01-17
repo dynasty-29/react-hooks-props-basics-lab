7a3de2568a0e66f747ce55dd0dc92367adc133cf
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-01-17 04:46:25 +0300

    props basic

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ function About(props) {
       <h2>About Me</h2>
       {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
       <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
